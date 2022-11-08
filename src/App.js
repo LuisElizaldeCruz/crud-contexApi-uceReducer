574aaf07123bfbdae9e229ff49ede5e8343bb96a
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-11-08 16:02:58 -0600

    se crearon las primeras rutas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Heading from "./components/Heading"
+import TaskForm from "./components/TaskForm"
+import TaskList from "./components/TaskList"
 
 function App() {
   return (
-    <div className="App">
-      <h1 className=''>hola mundo</h1>
+    <div>
+      <Heading />
+
+
+      <Routes>
+        <Route path="expenses" element={<TaskForm />} />
+        <Route path="invoices" element={<TaskList />} />
+      </Routes>
     </div>
   );
 }
